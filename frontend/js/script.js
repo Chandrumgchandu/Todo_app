4a5d151545a5949b9ead665f3bdc661c600f6e3a
Chandru M G <chandrumg124@gmail.com>
2026-06-09 12:23:31 +0000

    Added DevOps landing page

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,16 @@
-function showMsg() {
-    alert("Website is Live!");
+function showMessage() {
+    alert("✅ Deployment Successful! Website updated through Jenkins CI/CD Pipeline.");
 }
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        themeBtn.innerText = "☀️ Light Mode";
+    } else {
+        themeBtn.innerText = "🌙 Dark Mode";
+    }
+});
